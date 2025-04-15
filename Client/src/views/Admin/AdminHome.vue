a56b3ffe8ab4ef68c89e2d2410d8ad9249e41eed
voidrak <raklovetocode@gmail.com>
2025-04-15 16:23:06 +0300

    remove nodemodules

--- Client/src/views/Admin/AdminHome.vue
+++ Client/src/views/Admin/AdminHome.vue
@@ -1,7 +1,7 @@
 <script setup>
 import AdminLayout from "@/layout/AdminLayout.vue";
 import MonthlyTarget from "@/components/Ecommerce/MonthlyTarget.vue";
-import EcommerceMetrics from "@/components/Ecommerce/EcommerceMetrics.vue";
+import UsersMetrics from "@/components/Ecommerce/UsersMetrics.vue";
 import MonthlySale from "@/components/Ecommerce/MonthlySale.vue";
 import StatisticsChart from "@/components/Ecommerce/StatisticsChart.vue";
 import RecentOrders from "@/components/Ecommerce/RecentOrders.vue";
@@ -10,7 +10,7 @@ import RecentOrders from "@/components/Ecommerce/RecentOrders.vue";
   <admin-layout>
     <div class="grid grid-cols-12 gap-4 md:gap-6">
       <div class="col-span-12 space-y-6 xl:col-span-7">
-        <EcommerceMetrics/>
+        <UsersMetrics/>
         <MonthlySale />
       </div>
       <div class="col-span-12 xl:col-span-5">
@@ -21,11 +21,12 @@ import RecentOrders from "@/components/Ecommerce/RecentOrders.vue";
         <StatisticsChart />
       </div>
 
-      <div class="col-span-12 xl:col-span-5">
+      <div class="col-span-12 xl:col-span-6">
         
+        <RecentOrders />
       </div>
 
-      <div class="col-span-12 xl:col-span-7">
+      <div class="col-span-12 xl:col-span-6">
         <RecentOrders />
       </div>
     </div>
